dcc77e690996d095444e91e370c61fad700b368e
usuario <usuario@SVS151B12M>
2018-11-06 23:53:33 -0200

    avance test admin

--- Testing/src/Test/AdministradorTest1.java
+++ Testing/src/Test/AdministradorTest1.java
@@ -1,5 +1,11 @@
 package Test;
 
+import java.util.Date;
+
+import modelo.Cliente;
+
+import modelo.Colaborador;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
@@ -10,6 +16,7 @@ import org.junit.Test;
 public class AdministradorTest1
 {
     AdministradorTestFixture1 fixture1= new AdministradorTestFixture1();
+    
     public AdministradorTest1()
     {
     }
@@ -23,6 +30,7 @@ public class AdministradorTest1
     @After
     public void tearDown() throws Exception
     {
+        fixture1.tearDown();
     }
 
     @BeforeClass
@@ -39,19 +47,239 @@ public class AdministradorTest1
      * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
      */
     @Test
-    public void testSolicitarInformeCliente()
+    public void testSolicitarInformeCliente1()
     {
-        fail("Unimplemented");
+        try
+        {
+            Cliente c1= new Cliente("Juan","juan@gmail.com","4324234","2121321312","123","grupo1");
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,20);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeCliente(c1, d1, d2);
+            String mensaje= "Tarea de Servicio | Total horas  | Importe \n" + "Limpiar"+ " " + horas + " " + 100 + "\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente dispara excepcion");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeCliente2()
+    {
+        try
+        {
+            Cliente c2= new Cliente("Fran","fran@gmail.com","1234234","2134536546","222","grupo1");
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,10);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            double costo= (double) horas * 25;
+            String resultado= fixture1.admin.solicitarInformeCliente(c2, d1, d2);
+            String mensaje= "Tarea de Servicio | Total horas  | Importe \n" + "Cocinar"+ " " + horas + " " + costo + "\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente dispara excepcion");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeCliente3()
+    {
+        try
+        {
+            Cliente c1= new Cliente("Peter","peter@gmail.com","4324234","2121321312","123","grupo1");
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,20);
+            String resultado= fixture1.admin.solicitarInformeCliente(c1, d1, d2);
+            String mensaje= "Tarea de Servicio | Total horas  | Importe \n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente dispara excepcion");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeCliente4()
+    {
+        try
+        {
+            Cliente c1= new Cliente("Juan","juan@gmail.com","4324234","2121321312","123","grupo1");
+            Date d1= new Date(2018,9,1);
+            Date d2= new Date(2018,10,20);
+            String resultado= fixture1.admin.solicitarInformeCliente(c1, d1, d2);
+            String mensaje= "Tarea de Servicio | Total horas  | Importe \n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente dispara excepcion");
+        }
+    }
+    
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeCliente5()
+    {
+        try
+        {
+            Cliente c1= null;
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,20);
+            String resultado= fixture1.admin.solicitarInformeCliente(c1, d1, d2);
+            fail("No dispara excepcion por cliente null");
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente no contempla la excepcion cliente nulo");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeCliente(modelo.Cliente,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeCliente6()
+    {
+        try
+        {
+            Cliente c1= new Cliente("Juan","juan@gmail.com","4324234","2121321312","123","grupo1");
+            Date d1= null;
+            Date d2= null;
+            String resultado= fixture1.admin.solicitarInformeCliente(c1, d1, d2);
+            fail("No dispara excepcion por fecha null");
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeCliente no contempla fechas nulas");
+        }
     }
 
     /**
      * @see modelo.Administrador#solicitarInformeColaboradorIntervalo(modelo.Colaborador,java.util.Date,java.util.Date)
      */
     @Test
-    public void testSolicitarInformeColaboradorIntervalo()
+    public void testSolicitarInformeColaboradorIntervalo1()
     {
-        fail("Unimplemented");
+        try
+        {
+            Colaborador col1= new Colaborador("Pilar","pili@gmail.com","111111111","111A","1234","Colaborador");
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,30);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeColaboradorIntervalo(col1, d1, d2);
+            String mensaje= "Cliente  |  Tarea de servicio  | Total horas\n" + "Juan" + " " + "Limpiar" + " " + horas + "\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeColaboradorIntervalo dispara excepcion");
+        }
+        
     }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeColaboradorIntervalo(modelo.Colaborador,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeColaboradorIntervalo2()
+    {
+        try
+        {
+            Colaborador col1= new Colaborador("Peter","peter@gmail.com","111111111","111A","1234","Colaborador");
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,20);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeColaboradorIntervalo(col1, d1, d2);
+            String mensaje= "Cliente  |  Tarea de servicio  | Total horas\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeColaboradorIntervalo dispara excepcion");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeColaboradorIntervalo(modelo.Colaborador,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeColaboradorIntervalo3()
+    {
+        try
+        {
+            Colaborador col1= new Colaborador("Pilar","pili@gmail.com","111111111","111A","1234","Colaborador");
+            Date d1= new Date(2018,9,1);
+            Date d2= new Date(2018,10,20);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeColaboradorIntervalo(col1, d1, d2);
+            String mensaje= "Cliente  |  Tarea de servicio  | Total horas\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeColaboradorIntervalo dispara excepcion");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeColaboradorIntervalo(modelo.Colaborador,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeColaboradorIntervalo4()
+    {
+        try
+        {
+            Colaborador col1= null;
+            Date d1= new Date(2018,9,1);
+            Date d2= new Date(2018,10,20);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeColaboradorIntervalo(col1, d1, d2);
+            fail("No dispara excepcion por colaborador null");
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeColaboradorIntervalo no contempla colaborador null");
+        }
+    }
+    
+    /**
+     * @see modelo.Administrador#solicitarInformeColaboradorIntervalo(modelo.Colaborador,java.util.Date,java.util.Date)
+     */
+    @Test
+    public void testSolicitarInformeColaboradorIntervalo5()
+    {
+        try
+        {
+            Colaborador col1= new Colaborador("Peter","peter@gmail.com","111111111","111A","1234","Colaborador");
+            Date d1= null;
+            Date d2= null;
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarInformeColaboradorIntervalo(col1, d1, d2);
+            fail("No dispara excepcion por fecha null");
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarInformeColaboradorIntervalo no contempla fecha null");
+        }
+    }
+    
 
     /**
      * @see modelo.Administrador#solicitarTareasEnCursoColaboradores()
@@ -59,7 +287,21 @@ public class AdministradorTest1
     @Test
     public void testSolicitarTareasEnCursoColaboradores()
     {
-        fail("Unimplemented");
+        try
+        {
+            Date d1= new Date(2018,10,1);
+            Date d2= new Date(2018,10,30);
+            long horas= (d2.getTime() - d1.getTime())/3600000;
+            String resultado= fixture1.admin.solicitarTareasEnCursoColaboradores();
+            Colaborador c1= fixture1.admin.getBdd().getColaboradores().get(0);
+            String m1= "Pilar" + " " + "Juan" + " " + "Limpiar" + " " + d1 + " " + "cerrada";
+            String mensaje= " Colaborador   |   Cliente  | Servicio  | Inicio    | Estado    | Horas accumuladas | \n" + "" + "\n";
+            assertEquals("Informe incorrecto",resultado,mensaje);
+        }
+        catch( final Exception e )
+        {
+            fail("solicitarTareasEnCursoColaboradores dispara excepcion");
+        }
     }
 
     /**
--- Testing/src/Test/AdministradorTestFixture1.java
+++ Testing/src/Test/AdministradorTestFixture1.java
@@ -1,30 +1,66 @@
 package Test;
 
+import java.util.Date;
+
 import modelo.Administrador;
 import modelo.BaseDeDatos;
+import modelo.CerradaState;
 import modelo.Cliente;
+import modelo.Colaborador;
+import modelo.GrupoClientes;
+import modelo.HayTareaAbiertaException;
 import modelo.Servicio;
 
 public class AdministradorTestFixture1
 {
-    public Administrador admin=new Administrador();
-     
+     public Administrador admin=new Administrador();
 
      public AdministradorTestFixture1()
-     {this.admin.setBdd(new BaseDeDatos());
-         
+     {
+         this.admin.setBdd(new BaseDeDatos());
      }
 
      public void setUp()
      {
          this.admin.getBdd().setAdmin(admin);
-         this.admin.getBdd().getClientes().add(new Cliente("Juan","juan@gmail.com","4324234","2121321312","123","grupo1"));
-         this.admin.getBdd().getClientes().add(new Cliente("Fran","fran@gmail.com","1234234","2134536546","222","grupo3"));
-         this.admin.getBdd().getClientes().add(new Cliente("Nico","nico@gmail.com","54354234","12111112","333","grupo2"));
-         this.admin.getBdd().getClientes().add(new Cliente("Pedro","pedro@gmail.com","1124234","0123212","443","grupo2"));
-         this.admin.getBdd().getServicios().add(new Servicio("Limpiar","tipoA",52));
-         this.admin.getBdd().getServicios().add(new Servicio("Cocinar","tipoA",100));
-         this.admin.getBdd().getServicios().add(new Servicio("Barrer","tipoA",150));
+         Cliente c1= new Cliente("Juan","juan@gmail.com","4324234","2121321312","123","grupo1");
+         Cliente c2= new Cliente("Fran","fran@gmail.com","1234234","2134536546","222","grupo1");
+         Cliente c3= new Cliente("Pedro","pedro@gmail.com","1124234","0123212","443","grupo2");
+         this.admin.getBdd().getClientes().add(c1);
+         this.admin.getBdd().getClientes().add(c2);
+         this.admin.getBdd().getClientes().add(c3);
+         Servicio s1= new Servicio("Limpiar","Fijo",100);
+         Servicio s2= new Servicio("Cocinar","Por hora",25);
+         this.admin.getBdd().getServicios().add(s1);
+         this.admin.getBdd().getServicios().add(s2);
+         Colaborador col1= new Colaborador("Pilar","pili@gmail.com","111111111","111A","1234","Colaborador");
+         Colaborador col2= new Colaborador("Sanchez","sanchez@gmail.com","222222222","222B","2345","Colaborador");
+         this.admin.getBdd().getColaboradores().add(col1);
+         this.admin.getBdd().getColaboradores().add(col2);
+         GrupoClientes g1=new GrupoClientes();
+         GrupoClientes g2=new GrupoClientes();
+         g1.setNombre("grupo1");
+         g1.setNombre("grupo2");
+         this.admin.getBdd().getGrupos().add(g1);
+         this.admin.getBdd().getGrupos().add(g2);
+        try
+        {
+            this.admin.crearTarea(s1, c1, col1);
+        } catch (HayTareaAbiertaException e)
+        {
+        }
+        try
+        {
+            this.admin.crearTarea(s2, c2, col2);
+        } catch (HayTareaAbiertaException e)
+        {
+        }
+        col1.getTareas().get(c1).setFechainicio(new Date(2018,10,1));
+        col1.getTareas().get(c1).setFechacierre(new Date(2018,10,30));
+        col1.getTareas().get(c1).setEstado(new CerradaState(col1.getTareas().get(c1)));
+        col2.getTareas().get(c2).setFechainicio(new Date(2018,10,1));
+        col2.getTareas().get(c2).setFechacierre(new Date(2018,10,10));
+         col1.getTareas().get(c2).setEstado(new CerradaState(col1.getTareas().get(c2)));
      }
 
      public void tearDown()
@@ -34,3 +70,4 @@ public class AdministradorTestFixture1
          this.admin.getBdd().getGrupos().clear();
          this.admin.getBdd().getServicios().clear();
      }
+}
